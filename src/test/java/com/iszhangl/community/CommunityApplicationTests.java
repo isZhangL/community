958f83e648560493be4ebba953fcbf5e981b8eb9
volta <iszhangl@outlook.com>
2021-01-24 17:47:52 +0800

    init repo

---
+++ src/test/java/com/iszhangl/community/CommunityApplicationTests.java
@@ -0,0 +1,11 @@
+package com.iszhangl.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
